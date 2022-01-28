1427f34a178516402874d37407726da98063e9b1
jyouturner <jy2947@gmail.com>
2022-01-28 10:45:03 -0500

    add pip install

--- python/aws_helper.py
+++ python/aws_helper.py
@@ -4,7 +4,7 @@ import logging
 import boto3
 
 
-def load_secrets_to_environments():
+def load_secrets_to_environments(secret_id):
     try:
         client = boto3.client('secretsmanager')
         response = client.get_secret_value(
@@ -12,6 +12,8 @@ def load_secrets_to_environments():
         )
 
         oauth_credentials = json.loads(response['SecretString'])
+        # iterate each key-value pair and set to os environment variables
+        
         os.environ['SNAPDOCS_API_CLIENT_ID'] = oauth_credentials['client_id']
         os.environ['SNAPDOCS_API_CLIENT_SECRET'] = oauth_credentials['client_secret']
     except Exception as e:
